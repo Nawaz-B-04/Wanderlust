a442c73eafcba45f6f721eb36b23d75456075327
Nawaz B <nawazishb2004@gmail.com>
2025-01-02 11:45:03 +0530

    Updated show.ejs to display Mapbox map with dynamic coordinates

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,20 +1,18 @@
-const Listing = require("../models/listing")
-
-
-
+const Listing = require("../models/listing");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.Map_Token;
-const geocodingClient = mbxGeocoding({ accessToken: mapToken});
+const mapToken = process.env.Map_Token;  // Ensure that your Mapbox token is loaded from .env
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
-
-
+// Index Route - Show all listings
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings })
+    res.render("listings/index.ejs", { 
+        allListings, 
+        mapToken: process.env.Map_Token  // Pass the Mapbox token to the view
+    });
 }
-module.exports.renderNewForm = (req, res) => {
-    res.render("listings/new.ejs")
-}
+
+// Show specific listing page
 module.exports.showListing = async (req, res) => {
     let { id } = req.params;
 
@@ -24,36 +22,37 @@ module.exports.showListing = async (req, res) => {
             path: "author",
         },
     }).populate("owner");
+
     if (!listing) {
         req.flash("error", "Listing you requested for does not exist");
         return res.redirect("/listings");
     }
-    res.render("listings/show.ejs", { listing })
+    
+    // Pass the token to the view
+    res.render("listings/show.ejs", { 
+        listing, 
+        mapToken: process.env.Map_Token  // Pass token for map initialization
+    });
 }
 
-
+// Create a new listing (with geocoding)
 module.exports.createRoute = async (req, res, next) => {
     let response = await geocodingClient
     .forwardGeocode({
         query: req.body.listing.location,
         limit: 1
-      })
-        .send()
+    })
+    .send()
 
-
-        
     let url = req.file.path;
-    let filename = req.file.filename
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = { url, filename }
+    newListing.image = { url, filename };
     newListing.geometry = response.body.features[0].geometry;
-    
-    let saveListing = await newListing.save();
-    console.log(response.body);
 
-    console.log(saveListing)
-    req.flash("success", "New Listing Created!!!")
+    let saveListing = await newListing.save();
+    req.flash("success", "New Listing Created!!!");
     res.redirect("/listings");
 }
 // module.exports.createRoute = async (req, res, next) => {
